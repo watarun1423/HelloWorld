50e91e22ffa2c630a5934fcc54ce6e925efb0733
wataru tujimoto <wataru1423@icloud.com>
2018-10-11 11:59:34 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by tujimotowataru on 2018/10/11.
+//  Copyright © 2018年 Wataru Tujimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
